cf4b674aa3b7e12e3814a66e818b3d78e2d44dca
Ollie Pugh <pughor@outlook.com>
2023-10-03 23:13:19 +0100

    js enlarged avatar

--- app/javascript/controllers/select_avatar_controller.js
+++ app/javascript/controllers/select_avatar_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["avatar"];
+  static targets = ["avatar", "background"];
   connect() {
     console.log("bonjourno")
   }
@@ -22,15 +22,36 @@ export default class extends Controller {
       { id: 'user_profile_picture_thief_png', class: 'user_profile_picture_thief_png' },
       { id: 'user_profile_picture_woman_png', class: 'user_profile_picture_woman_png' }
     ];
-    for (const mapping of checkboxMappings) {
+    const checkboxColors = [
+      { class: 'cheerleader_color'},
+      { class: 'cowboy_color'},
+      { class: 'desert_color'},
+      { class: 'fisherman_color'},
+      { class: 'hip-hop_color'},
+      { class: 'lego-helmet_color'},
+      { class: 'lego-prof_color'},
+      { class: 'man_color'},
+      { class: 'ninja_color'},
+      { class: 'spiderman_color'},
+      { class: 'thief_color'},
+      { class: 'woman_color'}
+    ];
+    for (let i = 0; i < checkboxMappings.length && i < checkboxColors.length; i++) {
+      const mapping = checkboxMappings[i];
+      const color = checkboxColors[i];
+
       const checkbox = document.getElementById(mapping.id);
+
       if (checkbox) {
         if (checkbox.checked) {
           this.avatarTarget.classList.add(mapping.class);
+          this.backgroundTarget.classList.add(color.class);
         } else {
           this.avatarTarget.classList.remove(mapping.class);
+          this.backgroundTarget.classList.remove(color.class);
         }
       }
     }
+
   }
 }
